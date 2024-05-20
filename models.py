5e1b6a5c1a8276c648415657749646c029e07fdb
Fabián Abarca <ensinergia@gmail.com>
2024-05-19 21:06:59 -0300

    Colocar mejores nombres a los modelos y tareas de ejemplo. Algunos cambios al formato de la documentación.

--- models.py
+++ models.py
@@ -1,20 +1,27 @@
 from sqlalchemy import Column, Integer, Float, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import configparser
+
+# Datos de configuración
+config = configparser.ConfigParser()
+config.read("pipeline.cfg")
+system = config["db"]["system"]
+name = config["db"]["name"]
 
 # Crear la clase base de la tabla
 Base = declarative_base()
 
 
 # Definir los modelos
-class EnvironmentalSensor(Base):
-    __tablename__ = "environmental_sensor"
+class TestModel(Base):
+    __tablename__ = "test_table"
 
     timestamp = Column(Integer, primary_key=True)
 
 
-# Crear la conexión a la base de datos
-engine = create_engine("sqlite:///pipeline.db")
+# Crear la conexión a la base de datos SQLite3
+engine = create_engine(f"{system}:///{name}")
 Session = sessionmaker(bind=engine)
 session = Session()
 
--- tasks.py
+++ tasks.py
@@ -2,8 +2,17 @@ from celery import Celery
 from celery.schedules import timedelta
 from datetime import datetime
 import requests
+import configparser
 
-from models import session, EnvironmentalSensor
+from models import session, TestModel
+
+# Datos de configuración
+config = configparser.ConfigParser()
+config.read("pipeline.cfg")
+url = config["api"]["url"]
+token = config["api"]["token"]
+period = int(config["scheduler"]["period"])
+
 
 # Crear "app" de Celery
 app = Celery("tasks", broker="redis://localhost")
@@ -11,17 +20,20 @@ app = Celery("tasks", broker="redis://localhost")
 
 # Configurar las tareas de Celery
 @app.task
-def say_hello():
-    record = EnvironmentalSensor(timestamp=int(datetime.now().timestamp()))
+def test_task():
+    record = TestModel(timestamp=int(datetime.now().timestamp()))
     session.add(record)
     session.commit()
     return "Hello world!"
 
+# ----------
+# Configurar aquí las tareas de Celery para el procesamiento de los datos
+# ----------
 
 # Configurar el planificador de tareas de Celery
 app.conf.beat_schedule = {
-    "periodic-tester": {
-        "task": "tasks.say_hello",
-        "schedule": timedelta(seconds=10),
+    "test-schedule": {
+        "task": "tasks.test_task",
+        "schedule": timedelta(seconds=period),
     },
 }
